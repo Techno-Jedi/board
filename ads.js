556f12a5aed2ef677711f1851d6b435415d5be2d
Alex <aleksandr.leontev.n@mail.ru>
2022-12-07 20:45:43 +0300

    add style

--- ads.js
+++ ads.js
@@ -1,37 +1,46 @@
 (function (app) {
     app.PageAds = {
         draw:function (){
-            let content = document.querySelector(".content");
-            let divElementMain = document.createElement("div");
-            divElementMain.classList.add("boardAds")
-            content.append(divElementMain);
+
+            let divElementMain = createMainDiv();
 
             let divElement = document.createElement("div");
             divElement.classList.add("imageDescriptionPrice")
             divElementMain.append(divElement);
 
+            let imagesAndPhone = document.createElement("div");
+            imagesAndPhone.classList.add("imagesAndPhone");
+            divElement.append(imagesAndPhone);
+
             let img = document.createElement("div");
             img.classList.add("image")
-            divElement.append(img);
+            imagesAndPhone.append(img);
 
-            let divElementData = document.createElement("div");
-
-            divElementData.classList.add("data")
-            divElementMain.append(divElementData);
 
             let divElementPhone = document.createElement("div");
-            divElementData.append(divElementPhone);
+            imagesAndPhone.append(divElementPhone);
             divElementPhone.classList.add("phone")
             let elementP = document.createElement("p")
             divElementPhone.append(elementP);
             let numberPhone = document.createTextNode("Показать телефон");
             elementP.append(numberPhone)
 
+            let descriptionDivAndSalesman = document.createElement("div");
+            descriptionDivAndSalesman.classList.add("description-salesman");
+            divElement.append(descriptionDivAndSalesman);
+
             let descriptionDiv = document.createElement("div");
-            let description = document.createTextNode("Планшет Galaxy Tab. Немного потрепанный, цвет черный.");
-            descriptionDiv.append(description)
+            let descriptionP = document.createElement("p");
+            descriptionDiv.append(descriptionP)
+            let descriptionTitle =  document.createTextNode("Планшет Galaxy Tab.");
+            descriptionP.append(descriptionTitle)
+
+            let descriptionPd = document.createElement("p");
+            descriptionDiv.append(descriptionPd)
+            let description = document.createTextNode("Немного потрепанный, цвет черный.");
+            descriptionPd.append(description)
             descriptionDiv.classList.add("description")
-            divElement.append(descriptionDiv);
+            descriptionDivAndSalesman.append(descriptionDiv);
 
             let priceDiv = document.createElement("div");
             let price = document.createTextNode("3000 P");
@@ -40,15 +49,22 @@
             divElement.append(priceDiv);
 
             let divSalesman = document.createElement("div");
-            let salesman = document.createTextNode("Продавец:");
-            divSalesman.append(salesman)
             divSalesman.classList.add("salesman")
-            divElementData.append(divSalesman);
+            descriptionDivAndSalesman.append(divSalesman);
 
             let salesmanP = document.createElement("p")
             salesmanP.classList.add("surname")
             divSalesman.append(salesmanP);
-            let salesmanText = document.createTextNode("Гошан ГГ");
+            let salesmanText = document.createTextNode("Продавец:Гошан ГГ");
             salesmanP.append(salesmanText)
     }
-}})(AdsBoard);
+}
+        function createMainDiv() {
+            let content = document.querySelector(".content");
+            let divElementMain = document.createElement("div");
+            divElementMain.classList.add("boardAds")
+            content.append(divElementMain);
+            return divElementMain;
+        }
+
+})(AdsBoard);
